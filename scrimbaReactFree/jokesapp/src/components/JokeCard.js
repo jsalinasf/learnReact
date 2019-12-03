8be1b86c408b3df570468963af5afc40f2d4c6e5
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-03 01:28:48 -0500

    Scrimba - React Free - Final commit of the night

--- scrimbaReactFree/jokesapp/src/components/JokeCard.js
+++ scrimbaReactFree/jokesapp/src/components/JokeCard.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+// Check how to aplly inline style
+// using ternary operator or
+// using && operator
+
 const JokeCard = props => {
   return (
     <div
       className="jokeCard"
-      style={{
-        background: !props.joke && "linear-gradient(#ffffff, crimson)"
-      }}
+      style={{ background: !props.joke && "linear-gradient(#ffffff, crimson)" }}
     >
       <p style={{ display: props.joke ? "block" : "none" }}>
         Question: {props.joke}
