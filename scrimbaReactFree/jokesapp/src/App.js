d5afb2475b6f6ed9aca1b1c30b30a9104850e065
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-03 01:25:56 -0500

    Scrimba - React Free - Props exercise 01 - completed

--- scrimbaReactFree/jokesapp/src/App.js
+++ scrimbaReactFree/jokesapp/src/App.js
@@ -6,22 +6,22 @@ const App = () => {
     <div>
       <JokeCard
         joke={"What is the advantage of living in Switzerland?"}
-        punchline={"Well, the flag is a big plus!"}
+        punchLine={"Well, the flag is a big plus!"}
       />
       <JokeCard
         joke={"Why did Peter Pan fail as a stand-up comedian?"}
-        punchline={"Because his jokes Neverland"}
+        punchLine={"Because his jokes Neverland"}
       />
       <JokeCard
         joke={"What do you call an alligator wearing a vest?"}
-        punchline={"An investigator"}
+        punchLine={"An investigator"}
       />
       <JokeCard
         joke={"What did the termite say when he entered the bar?"}
-        punchline={"Is the bar tender here?"}
+        punchLine={"Is the bar tender here?"}
       />
       <JokeCard
-        punchline={
+        punchLine={
           "It’s hard to explain puns to kleptomaniacs because they always take things literally."
         }
       />
--- scrimbaReactFree/jokesapp/src/components/JokeCard.js
+++ scrimbaReactFree/jokesapp/src/components/JokeCard.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const JokeCard = props => {
   return (
-    <div className="jokeCard">
-      <p>Q: {props.joke}</p>
-      <p>R: {props.punchline}</p>
+    <div
+      className="jokeCard"
+      style={{
+        background: !props.joke && "linear-gradient(#ffffff, crimson)"
+      }}
+    >
+      <p style={{ display: props.joke ? "block" : "none" }}>
+        Question: {props.joke}
+      </p>
+      <p>Answer: {props.punchLine}</p>
     </div>
   );
 };
