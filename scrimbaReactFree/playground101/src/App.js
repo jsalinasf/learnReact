e3a6bb7462dce261ab908d7aa79bba1df2446c90
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-02 23:57:36 -0500

    Scrimba - React Free - Props 102

--- scrimbaReactFree/playground101/src/App.js
+++ scrimbaReactFree/playground101/src/App.js
@@ -3,6 +3,7 @@ import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import InlineStyle from "./components/InlineStyle";
+import ContactCard from "./components/ContactCard";
 
 const App = () => {
   return (
@@ -10,6 +11,18 @@ const App = () => {
       <Header />
       <MainContent />
       <InlineStyle />
+      <ContactCard
+        url={"http://www.randomkittengenerator.com/"}
+        name={"Mr Cat 01"}
+        phone={"438 569-6965"}
+        email={"cat1@hotmail.com"}
+      />
+      <ContactCard
+        url={"http://www.randomkittengenerator.com/"}
+        name={"Mr Cat 01"}
+        phone={"438 569-6965"}
+        email={"cat1@hotmail.com"}
+      />
       <Footer />
     </div>
   );
--- scrimbaReactFree/playground101/src/components/ContactCard.js
+++ scrimbaReactFree/playground101/src/components/ContactCard.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const ContactCard = props => {
   return (
-    <div>
-      <img src="{props.url}" className="cardFormat"></img>
+    <div className="cardFormat">
+      <img src="{props.url}" className="cardFormatImg"></img>
       <h3>{props.name}</h3>
       <p>Phone: {props.phone}</p>
       <p>Email: {props.email}</p>
