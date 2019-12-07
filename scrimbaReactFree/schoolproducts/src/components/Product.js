78f0c8c589e7e468e8eb283d9fec68b887911efc
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-07 01:14:09 -0500

    Scrimba - React Free - Practice School Products 102 - Adding buttons

--- scrimbaReactFree/schoolproducts/src/components/Product.js
+++ scrimbaReactFree/schoolproducts/src/components/Product.js
@@ -3,9 +3,15 @@ import React from "react";
 const Product = ({ product }) => {
   return (
     <div className="productCard">
-      <div className="productName">{product.name}</div>
-      <div className="productDescription">{product.description}</div>
-      <div className="productPrice">${product.price}</div>
+      <h2 className="productName">{product.name}</h2>
+      <p className="productDescription">{product.description}</p>
+      <p className="productPrice">
+        {product.price.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD"
+        })}
+      </p>
+      <button className="productButton">Get Info</button>
     </div>
   );
 };
