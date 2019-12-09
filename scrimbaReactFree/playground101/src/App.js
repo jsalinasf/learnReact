bf399ee3a2e5f0544c03519ba678408c06c4e287
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-09 00:05:39 -0500

    Scrimbra - React Free - Class Components Practice 01 - Class Component App and Header completed

--- scrimbaReactFree/playground101/src/App.js
+++ scrimbaReactFree/playground101/src/App.js
@@ -1,34 +1,63 @@
-import React from "react";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import InlineStyle from "./components/InlineStyle";
-import ContactCard from "./components/ContactCard";
+/*
 
-// const App = () => {
+Challenge:
+1. Convert all 3 components to be class-based
+2. Fix the small bug
+
+*/
+
+import React from "react";
+// import ReactDOM from "react-dom";
+
+// #1
+// function App() {
 //   return (
 //     <div>
-//       <Header />
-//       <MainContent />
-//       <ContactCard contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}/>
-//       <ContactCard contact={{name: "Fluffykins", imgUrl: "http://images6.fanpop.com/image/photos/40800000/socializing-kitten-300x200-cute-kittens-40835560-300-200.jpg", phone: "(809) 569-9986", email: "catcat@gmail.com"}} />
-//       <InlineStyle />
-//       <Footer />
+//       <p>Test</p>
+//       {/* <Header />
+//             <Greeting /> */}
 //     </div>
 //   );
-// };
+// }
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
-        <MainContent />
-        <ContactCard contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}} />
-        <Footer />
+        <Header2 name={"Mike"} />
       </div>
     );
   }
 }
+
+// // #2
+// function Header(props) {
+//   return (
+//     <header>
+//       <p>Welcome, {props.username}!</p>
+//     </header>
+//   );
+// }
+
+class Header2 extends React.Component {
+  render() {
+    return <p>Welcome, {this.props.name}!</p>;
+  }
+}
+// #3
+// function Greeting() {
+//   const date = new Date();
+//   const hours = date.getHours();
+//   let timeOfDay;
+
+//   if (hours < 12) {
+//     timeOfDay = "morning";
+//   } else if (hours >= 12 && hours < 17) {
+//     timeOfDay = "afternoon";
+//   } else {
+//     timeOfDay = "night";
+//   }
+//   return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+// }
 
 export default App;
---
+++ scrimbaReactFree/playground101/src/App_old.js
@@ -0,0 +1,34 @@
+import React from "react";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import InlineStyle from "./components/InlineStyle";
+import ContactCard from "./components/ContactCard";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <MainContent />
+//       <ContactCard contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}/>
+//       <ContactCard contact={{name: "Fluffykins", imgUrl: "http://images6.fanpop.com/image/photos/40800000/socializing-kitten-300x200-cute-kittens-40835560-300-200.jpg", phone: "(809) 569-9986", email: "catcat@gmail.com"}} />
+//       <InlineStyle />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <MainContent />
+        <ContactCard contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}} />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
