aec439e3838ac275dc887de4f5e9d2b72a049db7
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-09 00:16:51 -0500

    Scrimbra - React Free - Class Components Practice 01 - completed

--- scrimbaReactFree/playground101/src/App.js
+++ scrimbaReactFree/playground101/src/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
     return (
       <div>
         <Header2 name={"Mike"} />
+        <Greeting2 />
       </div>
     );
   }
@@ -41,7 +42,7 @@ class App extends React.Component {
 
 class Header2 extends React.Component {
   render() {
-    return <p>Welcome, {this.props.name}!</p>;
+    return <h1>Welcome, {this.props.name}!</h1>;
   }
 }
 // #3
@@ -59,5 +60,23 @@ class Header2 extends React.Component {
 //   }
 //   return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
 // }
+
+class Greeting2 extends React.Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay = "";
+
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "night";
+    }
+
+    return <h2>Good {timeOfDay} to you, sir or madam!</h2>;
+  }
+}
 
 export default App;
