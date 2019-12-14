b421ba5baffba2faf9e2121482cfd2ee93dede0a
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-13 23:30:04 -0700

    Scrimba - Free React - State 102 and conditoional rendering

--- scrimbaReactFree/playground101/src/App.js
+++ scrimbaReactFree/playground101/src/App.js
@@ -9,6 +9,9 @@ import React, { Component } from "react";
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      isLoggedIn: true
+    };
   }
   render() {
     return (
