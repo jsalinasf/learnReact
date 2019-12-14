7310d7d0283cc21166674ea9837123d48ab5d1cd
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-13 23:21:40 -0700

    Scrimba - Free React - State 101

--- scrimbaReactFree/playground101/src/App.js
+++ scrimbaReactFree/playground101/src/App.js
@@ -1,81 +1,25 @@
-/*
-
-Challenge:
-1. Convert all 3 components to be class-based
-2. Fix the small bug
-
-*/
-
 import React from "react";
-// import ReactDOM from "react-dom";
 
-// #1
-// function App() {
-//   return (
-//     <div>
-//       <p>Test</p>
-//       {/* <Header />
-//             <Greeting /> */}
-//     </div>
-//   );
-// }
+// Challenge:
+// Given an incomplete class-based component without a constructor,
+// add a constructor and initialize state to fix the broken component.
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Angela",
+      age: 38
+    };
+  }
+
   render() {
     return (
       <div>
-        <Header2 name={"Mike"} />
-        <Greeting2 />
+        <h1>{this.state.name}</h1>
+        <h3>{this.state.age} years old</h3>
       </div>
     );
-  }
-}
-
-// // #2
-// function Header(props) {
-//   return (
-//     <header>
-//       <p>Welcome, {props.username}!</p>
-//     </header>
-//   );
-// }
-
-class Header2 extends React.Component {
-  render() {
-    return <h1>Welcome, {this.props.name}!</h1>;
-  }
-}
-// #3
-// function Greeting() {
-//   const date = new Date();
-//   const hours = date.getHours();
-//   let timeOfDay;
-
-//   if (hours < 12) {
-//     timeOfDay = "morning";
-//   } else if (hours >= 12 && hours < 17) {
-//     timeOfDay = "afternoon";
-//   } else {
-//     timeOfDay = "night";
-//   }
-//   return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
-// }
-
-class Greeting2 extends React.Component {
-  render() {
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay = "";
-
-    if (hours < 12) {
-      timeOfDay = "morning";
-    } else if (hours >= 12 && hours < 17) {
-      timeOfDay = "afternoon";
-    } else {
-      timeOfDay = "night";
-    }
-
-    return <h2>Good {timeOfDay} to you, sir or madam!</h2>;
   }
 }
 
---
+++ scrimbaReactFree/playground101/src/App03.js
@@ -0,0 +1,87 @@
+/*
+
+Challenge:
+1. Convert all 3 components to be class-based
+2. Fix the small bug
+
+*/
+
+import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+
+// #1
+// function App() {
+//   return (
+//     <div>
+//       <p>Test</p>
+//       {/* <Header />
+//             <Greeting /> */}
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header2 name={"Mike"} />
+        <Greeting2 />
+      </div>
+    );
+  }
+}
+
+// // #2
+// function Header(props) {
+//   return (
+//     <header>
+//       <p>Welcome, {props.username}!</p>
+//     </header>
+//   );
+// }
+
+class Header2 extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Welcome, {this.props.name}!</h1>;
+      </header>
+    );
+  }
+}
+// #3
+// function Greeting() {
+//   const date = new Date();
+//   const hours = date.getHours();
+//   let timeOfDay;
+
+//   if (hours < 12) {
+//     timeOfDay = "morning";
+//   } else if (hours >= 12 && hours < 17) {
+//     timeOfDay = "afternoon";
+//   } else {
+//     timeOfDay = "night";
+//   }
+//   return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+// }
+
+// Check line 9, import { Component }
+class Greeting2 extends Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay = "";
+
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "night";
+    }
+
+    return <h2>Good {timeOfDay} to you, sir or madam!</h2>;
+  }
+}
+
+export default App;
