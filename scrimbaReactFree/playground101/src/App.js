34dbcbfe11bab61d2bda6f67b5b95592a85c5aad
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-16 23:54:37 -0500

    Scrimba - React Free - Managing Events 101 - mouse over

--- scrimbaReactFree/playground101/src/App.js
+++ scrimbaReactFree/playground101/src/App.js
@@ -3,12 +3,12 @@ import React from "react";
 function App() {
   return (
     <div>
-      <img src="https://www.fillmurray.com/200/100" alt="Bill smoking a pipe" />
+      <img src="https://www.fillmurray.com/200/100" alt="Bill smoking a pipe" onMouseOver={function() {console.log('Mouse over the image')}}/>
       <br />
       <br />
       <button
         onClick={function() {
-          console.log("Test");
+          console.log("Button clicked");
         }}
       >
         Click Me!
