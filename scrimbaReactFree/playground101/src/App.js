2140f0cb68e23c2bb4815f37af8c0ab5d7b1c66b
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-17 09:34:41 -0500

    Scrimba - React Free - Changing state with events 101

--- scrimbaReactFree/playground101/src/App.js
+++ scrimbaReactFree/playground101/src/App.js
@@ -6,17 +6,25 @@ class App extends Component {
     this.state = {
       count: 0
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick = () => {
-    alert("click");
-  };
+  handleClick() {
+    // this.setState must return an object
+    // prevState is the state of the component before the event
+    // State can not be modified directly
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      };
+    });
+  }
 
   render() {
     return (
       <div>
         <h1>{this.state.count}</h1>
-        <button onClick={this.handleClick}>Change</button>
+        <button onClick={this.handleClick}>Change!</button>
       </div>
     );
   }
