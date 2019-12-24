65cb79e52176cb079469139808a1719a8a96bfe6
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-24 00:10:02 -0500

    Scrimba - Free React - updated CSS

--- scrimbaReactFree/playground101/src/components/MainContent.js
+++ scrimbaReactFree/playground101/src/components/MainContent.js
@@ -2,6 +2,14 @@ import React from "react";
 import image01 from "../img/spiderman01.jpg";
 
 const MainContent = () => {
+  let dynamicStyle = {
+    border: "5px solid red",
+    display: "block",
+    position: "relative",
+    left: "50%",
+    transform: "translate(-50%, 0)"
+  };
+
   return (
     <div>
       <p>
@@ -9,7 +17,7 @@ const MainContent = () => {
         and writer-artist Steve Ditko. He first appeared in the anthology comic
         book Amazing Fantasy #15 in the Silver Age of Comic Books
       </p>
-      <img src={image01} alt="Spider-Man"></img>
+      <img src={image01} alt="Spider-Man" style={dynamicStyle}></img>
       <p>
         For more information please visit:{" "}
         <a href="https://en.wikipedia.org/wiki/Spider-Man">Wikipedia Link</a>
