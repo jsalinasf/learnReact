79543fd6b11fda670e5daf70962b68a1d21bfe94
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-24 21:20:06 -0500

    Scrimba - Free React - To Do App phase 7 - Completed following instructor solution

--- scrimbaReactFree/reacttodo/src/components/TodoItem.js
+++ scrimbaReactFree/reacttodo/src/components/TodoItem.js
@@ -1,58 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 
-// const TodoItem = props => {
-//   return (
-//     <div className="todoItem">
-//       <input
-//         type="checkbox"
-//         name={props.item.id}
-//         id={props.item.id}
-//         // defaultChecked={props.item.completed}
-//         checked={props.item.completed}
-//         onChange={() => props.handleChange(props.item.id)}
-//       ></input>
-//       <label htmlFor={props.item.id}>{props.item.text}</label>
-//     </div>
-//   );
-// };
-
-// export default TodoItem;
-
-class TodoItem extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      pendingStyle: {
-        textDecoration: "none",
-        color: "#333333"
-      },
-      completedStyle: {
-        textDecoration: "line-through",
-        color: "#cecece"
-      }
-    };
-  }
-
-  render() {
-    const itemLabelStyle = this.props.item.completed
-      ? this.state.completedStyle
-      : this.state.pendingStyle;
-
-    return (
-      <div className="todoItem">
-        <input
-          type="checkbox"
-          name={this.props.item.id}
-          id={this.props.item.id}
-          checked={this.props.item.completed}
-          onChange={() => this.props.handleChange(this.props.item.id)}
-        ></input>
-        <label style={itemLabelStyle} htmlFor={this.props.item.id}>
-          {this.props.item.text}
-        </label>
-      </div>
-    );
-  }
-}
+const TodoItem = props => {
+  const completedItemStyle = {
+    textDecoration: "line-through",
+    color: "#cecece"
+  };
+  return (
+    <div className="todoItem">
+      <input
+        type="checkbox"
+        name={props.item.id}
+        id={props.item.id}
+        // defaultChecked={props.item.completed}
+        checked={props.item.completed}
+        onChange={() => props.handleChange(props.item.id)}
+      ></input>
+      <label
+        style={props.item.completed ? completedItemStyle : null}
+        htmlFor={props.item.id}
+      >
+        {props.item.text}
+      </label>
+    </div>
+  );
+};
 
 export default TodoItem;
+
+// import React, { Component } from "react";
+// class TodoItem extends Component {
+//   constructor(props) {
+//     super();
+//     this.state = {
+//       pendingStyle: {
+//         textDecoration: "none",
+//         color: "#333333"
+//       },
+//       completedStyle: {
+//         textDecoration: "line-through",
+//         color: "#cecece"
+//       }
+//     };
+//   }
+
+//   render() {
+//     const itemLabelStyle = this.props.item.completed
+//       ? this.state.completedStyle
+//       : this.state.pendingStyle;
+
+//     return (
+//       <div className="todoItem">
+//         <input
+//           type="checkbox"
+//           name={this.props.item.id}
+//           id={this.props.item.id}
+//           checked={this.props.item.completed}
+//           onChange={() => this.props.handleChange(this.props.item.id)}
+//         ></input>
+//         <label style={itemLabelStyle} htmlFor={this.props.item.id}>
+//           {this.props.item.text}
+//         </label>
+//       </div>
+//     );
+//   }
+// }
+
+// export default TodoItem;
