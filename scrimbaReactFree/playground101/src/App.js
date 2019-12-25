0a9d78bd447a3e311678921557eeed3a65fb3703
jsalinasf <jorge.salinas.jsf@outlook.com>
2019-12-24 20:03:11 -0500

    Scrimba - Free React - Conditional Rendering Practice 102

--- scrimbaReactFree/playground101/src/App.js
+++ scrimbaReactFree/playground101/src/App.js
@@ -4,16 +4,26 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      isUserLoggedIn: false
+      isUserLoggedIn: true
     };
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick() {
+    alert("Test");
+  }
+
   render() {
     return (
       <div>
+        <h1>Conditional Rendering and State React Practice</h1>
         <div>
           User's state: {this.state.isUserLoggedIn ? "logged in" : "logged out"}
         </div>
-        <button>{this.state.isUserLoggedIn ? "Log Out" : "Log In"}</button>
+        <br />
+        <button onClick={this.handleClick}>
+          {this.state.isUserLoggedIn ? "Log Out" : "Log In"}
+        </button>
       </div>
     );
   }
