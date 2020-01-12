52aa8f6e1572e21dbab71aa53f6b0ad864f731ed
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-01-12 14:01:06 -0500

    Scrimba - React Free - Forms and Controlled Components 101

--- scrimbaReactFree/reactforms/src/App.js
+++ scrimbaReactFree/reactforms/src/App.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
-    return <div>Test</div>;
+    return (
+      <div>
+        <h1>
+          {this.state.firstName} {this.state.lastName}
+        </h1>
+        <form>
+          <label for="firstName">First Name: </label>
+          <input
+            name="firstName"
+            id="firstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+          ></input>
+          <br />
+          <label for="lastName">Last Name: </label>
+          <input
+            name="lastName"
+            id="lastName"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+          ></input>
+        </form>
+      </div>
+    );
   }
 }
 
