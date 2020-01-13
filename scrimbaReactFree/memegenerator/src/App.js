0ea21f72cec2e3c19259c95152317f4ba04964c1
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-01-13 12:44:17 -0500

    Scrimbra - React Free - Meme Generator boilerplate

---
+++ scrimbaReactFree/memegenerator/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+export default App;
