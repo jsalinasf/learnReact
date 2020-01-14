b2711b095de797aeb83dff7065a43d2c39f585d4
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-01-13 21:14:51 -0500

    Scrimba - React Free - useEffect 103

--- scrimbaReactFree/reacthooks/src/App.js
+++ scrimbaReactFree/reacthooks/src/App.js
@@ -2,11 +2,25 @@ import React, { useState, useEffect } from "react";
 import randomcolor from "randomcolor";
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("");
+
+  // useEffect run everytime the component changes!
+  // I can have multiple useEffects
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount(prevCounter => prevCounter + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    setColor(randomcolor());
+  }, [count]);
 
   return (
     <div>
-      <h1>{counter}</h1>
+      <h1 style={{ color: color }}>{count}</h1>
       <button>Start</button>
     </div>
   );
