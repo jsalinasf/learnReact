a56ae4cee2aab2457436b50a0a1c1992937503ad
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-01-20 23:53:56 -0500

    Scrimba - React PRO - React Defragment 102

--- scrimbaReactPro/fragment/src/components/Child.js
+++ scrimbaReactPro/fragment/src/components/Child.js
@@ -1,10 +1,12 @@
 import React from "react";
+import GrandChild from "./GrandChild";
 
 const Child = () => {
   return (
     <div>
       <h2>Child Component</h2>
       <p>Im in the Child Component</p>
+      <GrandChild />
     </div>
   );
 };
--- scrimbaReactPro/fragment/src/components/GrandChild.js
+++ scrimbaReactPro/fragment/src/components/GrandChild.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+const GrandChild = () => {
+  return (
+    <div>
+      <h3>GrandChild Component</h3>
+      <p>Im in the GrandChild Component</p>
+    </div>
+  );
+};
+
+export default GrandChild;
