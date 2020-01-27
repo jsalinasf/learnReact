202bfd7951fff7a5322490df0f69ca4852528c17
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-01-26 20:50:52 -0500

    Scrimba PRO React - PropTypes - practice 02 completed

--- scrimbaReactPro/01-Introduction/defaultprops/src/App.js
+++ scrimbaReactPro/01-Introduction/defaultprops/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import FunctionalCard from "./components/FunctionalCard";
 import ClassCard from "./components/ClassCard";
+import RoundedImg from "./components/RoundedImg";
 
 const App = () => {
   return (
     <div>
-      <h1>React Default Props</h1>
+      <h1>React Default Props and TypeProps</h1>
+      <p>PropTypes exercise</p>
+      <RoundedImg
+        src="https://picsum.photos/id/237/300/300"
+        borderRadius="10px"
+      />
       <p>Here are 3 FunctionalCards components:</p>
       <FunctionalCard backgroundColor={"black"} width={100} height={"mike"} />
       <FunctionalCard backgroundColor={"blue"} width={150} height={150} />
