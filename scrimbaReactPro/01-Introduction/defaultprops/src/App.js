cd1e0cccb1c7fa6332ad7978e5b5169b67d76823
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-01-26 20:32:29 -0500

    Scrimba PRO React - PropTypes - practice 01 completed

--- scrimbaReactPro/01-Introduction/defaultprops/src/App.js
+++ scrimbaReactPro/01-Introduction/defaultprops/src/App.js
@@ -7,12 +7,12 @@ const App = () => {
     <div>
       <h1>React Default Props</h1>
       <p>Here are 3 FunctionalCards components:</p>
-      <FunctionalCard backgroundColor={"black"} width={100} height={100} />
-      <FunctionalCard backgroundColor={"green"} width={150} height={150} />
+      <FunctionalCard backgroundColor={"black"} width={100} height={"mike"} />
+      <FunctionalCard backgroundColor={"blue"} width={150} height={150} />
       <FunctionalCard />
       <p>Here are 3 ClassCards components:</p>
-      <ClassCard backgroundColor={"black"} width={100} height={100} />
-      <ClassCard backgroundColor={"green"} width={150} height={150} />
+      <ClassCard backgroundColor={"red"} width={100} height={100} />
+      <ClassCard backgroundColor={"blue"} width={150} height={150} />
       <ClassCard />
     </div>
   );
--- scrimbaReactPro/01-Introduction/defaultprops/src/components/FunctionalCard.js
+++ scrimbaReactPro/01-Introduction/defaultprops/src/components/FunctionalCard.js
@@ -10,15 +10,18 @@ const Card = props => {
   return <div className={"card"} style={styles}></div>;
 };
 
+// Please watch the broswer console window to see the alerts
+// Remember, PropTypes only works on Development mode
 Card.propTypes = {
-  backgroundColor: PropTypes.string.isRequired,
-  width: PropTypes.number.isRequired
+  backgroundColor: PropTypes.oneOf(["red", "blue"]),
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number
 };
 
-Card.defaultProps = {
-  // backgroundColor: "crimson",
-  // width: 50,
-  // height: 50
-};
+// Card.defaultProps = {
+//   backgroundColor: "crimson",
+//   width: 50,
+//   height: 50
+// };
 
 export default Card;
