e3034f627edff52a3cb391f5ec7d4e6c20ecae22
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-01-28 17:53:05 -0500

    Scrimba React PRO - Children Component

---
+++ scrimbaReactPro/02-Reusability/children-components/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>
+        This is one the best looking website you have ever visited int the
+        internet
+      </p>
+    </div>
+  );
+};
+
+export default App;
