789513dfbae853e8977603ab1a01b2f74f5e27c0
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-02-04 23:42:49 -0500

    Scrimbra React Pro - High Order Components - boilerplate complete

---
+++ scrimbaReactPro/02-Reusability/high-order-components/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <h1>Test</h1>
+      <p>Validating basic functionality</p>
+    </div>
+  );
+};
+
+export default App;
