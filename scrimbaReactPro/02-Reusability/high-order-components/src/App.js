6f8644ff2da4d133e809d9fd51e0f342114c1601
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-02-05 00:05:45 -0500

    Scrimbra React Pro - High Order Components - practice 01 completed

--- scrimbaReactPro/02-Reusability/high-order-components/src/App.js
+++ scrimbaReactPro/02-Reusability/high-order-components/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { withFavoriteNumber } from './components/withFavoriteNumber';
 
-const App = () => {
+const App = props => {
   return (
     <div className='container'>
-      <h1>Test</h1>
-      <p>Validating basic functionality</p>
+      <h1>High Order Components</h1>
+      <p>
+        A function that takes a component as its first argument and returns a
+        new component that wraps the given component, providing extra
+        capabilities to it.
+      </p>
+      <br />
+      <div>{props.favoriteNumber}</div>
     </div>
   );
 };
 
-export default App;
+// export default App;
+export default withFavoriteNumber(App);
