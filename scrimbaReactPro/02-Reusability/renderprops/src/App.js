28796afd462d9eb867b165a5f8266993f6e04480
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-02-12 22:57:07 -0500

    Scrimba React Prop - Render Props bolierplate

---
+++ scrimbaReactPro/02-Reusability/renderprops/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Example from './components/Example';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Render Props</h1>
+      <Example />
+    </div>
+  );
+};
+
+export default App;
---
+++ scrimbaReactPro/02-Reusability/renderprops/src/components/Example.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Example = props => {
+  return (
+    <div>
+      <h2>Example Component</h2>
+      <p>Render Props</p>
+    </div>
+  );
+};
+
+export default Example;
