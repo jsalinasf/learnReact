413e955e8d247de32c02a138bb091c1aef442bd6
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-02-18 23:02:36 -0500

    Scrimba PRO - Class PureComponent

--- scrimbaReactPro/03-performance/classcomponent-shouldupdate/src/App.js
+++ scrimbaReactPro/03-performance/classcomponent-shouldupdate/src/App.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import GrandParent from './components/GrandParent';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+    this.incrementCount = this.incrementCount.bind(this);
+  }
+
+  incrementCount() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      };
+    });
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.incrementCount}>{this.state.count}</button>
         <h1>App Component</h1>
         <GrandParent />
         <GrandParent />
