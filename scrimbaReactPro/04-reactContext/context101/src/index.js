cf6e26529b32f07d569755026ce25427981029ea
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-02-26 23:36:34 -0500

    Scrimba PRO React - ThemeContext and Provider 101

--- scrimbaReactPro/04-reactContext/context101/src/index.js
+++ scrimbaReactPro/04-reactContext/context101/src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ThemeContext = React.createContext();
+// compound component:
+// ThemeContext.Provider & ThemeContext.Consumer
+
+ReactDOM.render(
+  <ThemeContext.Provider value={'light'}>
+    <App />
+  </ThemeContext.Provider>,
+  document.getElementById('root')
+);
