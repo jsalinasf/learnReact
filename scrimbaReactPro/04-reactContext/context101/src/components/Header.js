0b8edc28f8c10a4e6a193e1bee4d76e0aa14361a
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-02-26 23:31:50 -0500

    Scrimba PRO React - React Context boilerplate 101

---
+++ scrimbaReactPro/04-reactContext/context101/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header className='light-Theme'>
+          <h2>Light Theme</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
