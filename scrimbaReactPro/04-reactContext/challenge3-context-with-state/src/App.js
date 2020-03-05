c1bb45b0d6985da38b0f8a4174724c03351a468f
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-03-05 17:36:13 -0500

    Scrimba PRO React - Practice 3 - context with state 102, main complete

--- scrimbaReactPro/04-reactContext/challenge3-context-with-state/src/App.js
+++ scrimbaReactPro/04-reactContext/challenge3-context-with-state/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
+import {UserContextConsumer} from './userContext'
 
 function App() {
-  return <Header />;
+  return (
+    <Header />
+    <main>
+      <UserContextConsumer>
+        {context => (
+          <p className="main">No new notifications, {context.username}! 🎉</p>
+        )}
+      </UserContextConsumer>
+
+    </main>
+  )
 }
 
 export default App;
