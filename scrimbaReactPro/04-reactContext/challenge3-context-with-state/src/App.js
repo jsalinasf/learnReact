c475da781cd9ebe6203e466bd00c77a1599af918
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-03-05 17:43:19 -0500

    Scrimba PRO React - Practice 3 - context with state. Button is updating context state. Now I should work on the form

--- scrimbaReactPro/04-reactContext/challenge3-context-with-state/src/App.js
+++ scrimbaReactPro/04-reactContext/challenge3-context-with-state/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import Header from './components/Header';
-import {UserContextConsumer} from './userContext'
+import { UserContextConsumer } from './userContext';
 
 function App() {
   return (
-    <Header />
-    <main>
-      <UserContextConsumer>
-        {context => (
-          <p className="main">No new notifications, {context.username}! 🎉</p>
-        )}
-      </UserContextConsumer>
-
-    </main>
-  )
+    <div>
+      <Header />
+      <main>
+        <UserContextConsumer>
+          {context => (
+            <div>
+              <p className='main'>
+                No new notifications, {context.username}! 🎉
+              </p>
+              <button onClick={context.updateUsername}>Update Username</button>
+            </div>
+          )}
+        </UserContextConsumer>
+      </main>
+    </div>
+  );
 }
 
 export default App;
