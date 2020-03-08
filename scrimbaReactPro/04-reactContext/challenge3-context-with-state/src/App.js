c58d3befa9286665973ad11bbde2d58317309e06
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-03-07 19:41:39 -0500

    Scrimba React PRO - App component switched back to class component

--- scrimbaReactPro/04-reactContext/challenge3-context-with-state/src/App.js
+++ scrimbaReactPro/04-reactContext/challenge3-context-with-state/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
 import { UserContextConsumer } from './userContext';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <UserContextConsumer>
-          {context => (
-            <div>
-              <p className='main'>
-                No new notifications, {context.username}! 🎉
-              </p>
-              <button onClick={context.updateUsername}>Update Username</button>
-            </div>
-          )}
-        </UserContextConsumer>
-      </main>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <main>
+          <UserContextConsumer>
+            {context => (
+              <div>
+                <p className='main'>
+                  No new notifications, {context.username}! 🎉
+                </p>
+                <button onClick={context.updateUsername}>
+                  Update Username
+                </button>
+              </div>
+            )}
+          </UserContextConsumer>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
