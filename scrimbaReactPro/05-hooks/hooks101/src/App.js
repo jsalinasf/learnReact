dcaf92f5abee2b4576af3d38ebff784999dbbf3a
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-03-16 00:39:13 -0500

    Scrimba React Pro - Modifying state without changing previous state 102

--- scrimbaReactPro/05-hooks/hooks101/src/App.js
+++ scrimbaReactPro/05-hooks/hooks101/src/App.js
@@ -15,14 +15,26 @@ function App() {
     setCounter(prevCounter => prevCounter - 1);
   }
 
+  function handleDoubleCounter() {
+    setCounter(prevCounter => prevCounter * 2);
+  }
+
+  function handleHalfCounter() {
+    setCounter(prevCounter => Math.ceil(prevCounter / 2));
+  }
+
   return (
     <div>
       <h1>{counter}</h1>
+      <button onClick={handleDoubleCounter}>Double</button>
+      <br />
       <button onClick={handleIncreaseCounter}>Increase</button>
       <br />
       <button onClick={handleResetCounter}>Reset</button>
       <br />
       <button onClick={handleDecreaseCounter}>Decrease</button>
+      <br />
+      <button onClick={handleHalfCounter}>Half</button>
     </div>
   );
 }
