4c5241578540a8d5043ce64c5c03e59b549ce3c5
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-03-21 21:23:48 -0500

    Scrimba React PRO - Typing Game - Markup completed

--- scrimbaReactPro/05-hooks/projecthooks1/src/App.js
+++ scrimbaReactPro/05-hooks/projecthooks1/src/App.js
@@ -3,8 +3,11 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Typing Game</h1>
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h4>Time remaining: ?</h4>
       <button>Start</button>
+      <h1>Word Count: ?</h1>
     </div>
   );
 }
