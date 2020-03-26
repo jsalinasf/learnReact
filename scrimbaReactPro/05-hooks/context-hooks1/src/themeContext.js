c81ada6225318e3a3a878f48e4bb5a9358f8482a
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-03-26 00:07:32 -0500

    Scrima React Pro - useContext migrated to Functional COmponent

--- scrimbaReactPro/05-hooks/context-hooks1/src/themeContext.js
+++ scrimbaReactPro/05-hooks/context-hooks1/src/themeContext.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 const ThemeContext = React.createContext();
 
-class ThemeContextProvider extends Component {
-  state = {
-    theme: 'dark'
-  };
+function ThemeContextProvider(props) {
+  const [theme, setTheme] = useState('dark');
 
-  toggleTheme = () => {
-    this.setState(prevState => {
-      return {
-        theme: prevState.theme === 'light' ? 'dark' : 'light'
-      };
-    });
-  };
-
-  render() {
-    return (
-      <ThemeContext.Provider
-        value={{ theme: this.state.theme, toggleTheme: this.toggleTheme }}
-      >
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
+  function toggleTheme() {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
   }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
 }
 
 export { ThemeContextProvider, ThemeContext };
+
+// class ThemeContextProvider extends Component {
+//   state = {
+//     theme: 'dark'
+//   };
+
+//   toggleTheme = () => {
+//     this.setState(prevState => {
+//       return {
+//         theme: prevState.theme === 'light' ? 'dark' : 'light'
+//       };
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <ThemeContext.Provider
+//         value={{ theme: this.state.theme, toggleTheme: this.toggleTheme }}
+//       >
+//         {this.props.children}
+//       </ThemeContext.Provider>
+//     );
+//   }
+// }
