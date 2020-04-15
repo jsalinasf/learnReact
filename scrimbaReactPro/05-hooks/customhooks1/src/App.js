681c7c51972d9536761d624ea09680d142e0c388
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-04-14 23:14:33 -0500

    Scrimba React PRO - Custom Hoooks 103 - returning options from custom hooks (array vs object)

--- scrimbaReactPro/05-hooks/customhooks1/src/App.js
+++ scrimbaReactPro/05-hooks/customhooks1/src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+// Custom Hooks
+
+import React from 'react';
+import useCounter from './useCounter';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // Option 1: Destructuring as an array
+  // Flexible because I can use whatever names I want since arrays manages indexes
+  const [number, add] = useCounter();
 
-  function increment() {
-    setCount((prevCount) => prevCount + 1);
-  }
+  // Option 2: Destructuring as an object
+  // Not very flexible because the component MUST use the exact names the custom hook is returning
+  // const { count, increment } = useCounter();
 
   return (
     <div>
-      <h1>The count is: {count}</h1>
-      <button onClick={increment}>Add 1</button>
+      <h1>The count is: {number}</h1>
+      <button onClick={add}>Add 1</button>
     </div>
   );
 }
--- scrimbaReactPro/05-hooks/customhooks1/src/useCounter.js
+++ scrimbaReactPro/05-hooks/customhooks1/src/useCounter.js
@@ -11,7 +11,16 @@ function useCounter() {
   }
 
   // 3. Return whatever we want another component to have access to (count, increment)
-  return { count, increment };
+
+  // There are two ways to do it
+
+  // Option 1: returning an array, more flexible because the component can name the
+  // returned values as he pleases
+  return [count, increment];
+
+  // Option 2: returning an object
+  // Not very flexible because the component MUST use the exact names my object is returning
+  // return { count, increment };
 }
 
 export default useCounter;
