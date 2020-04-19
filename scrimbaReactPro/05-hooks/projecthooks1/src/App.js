f3c37eea030afe234aeb0c506587cab0b4b48fbc
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-04-18 22:12:11 -0500

    Scrimba React PRO - Typing game using custom hooks

--- scrimbaReactPro/05-hooks/projecthooks1/src/App.js
+++ scrimbaReactPro/05-hooks/projecthooks1/src/App.js
@@ -1,49 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
+import useWordGame from './hooks/useWordGame';
 
 function App() {
-  const STARTING_TIME = 3;
-  const [text, setText] = useState('');
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
-  const [isTimeRunning, setIsTimeRunning] = useState(false);
-  const [wordCount, setWordCount] = useState(0);
-  const textBoxRef = useRef(null);
-
-  function handleChange(e) {
-    const { value } = e.target;
-    setText(value);
-  }
-
-  function calculateWordCount(text) {
-    const wordsArr = text.trim().split(' ');
-    return wordsArr.filter(word => word !== '').length;
-  }
-
-  function decrementTimeRemaining() {
-    setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 1);
-  }
-
-  function startGame() {
-    setIsTimeRunning(true);
-    setTimeRemaining(STARTING_TIME);
-    setWordCount(0);
-    setText('');
-    textBoxRef.current.disabled = false;
-    textBoxRef.current.focus();
-  }
-
-  function endGame() {
-    setIsTimeRunning(false);
-    setWordCount(calculateWordCount(text));
-  }
-
-  useEffect(() => {
-    if (timeRemaining > 0 && isTimeRunning) {
-      setTimeout(decrementTimeRemaining, 1000);
-    } else if (timeRemaining === 0) {
-      endGame();
-    }
-  }, [timeRemaining, isTimeRunning]);
-
+  const {
+    text,
+    timeRemaining,
+    isTimeRunning,
+    wordCount,
+    textBoxRef,
+    handleChange,
+    startGame,
+  } = useWordGame(5);
   return (
     <div>
       <h1>How fast do you type?</h1>
