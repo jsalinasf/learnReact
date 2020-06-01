40114cf49214922d1bc434afa3dbbf6b1cfe9347
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-05-31 23:22:24 -0500

    React PRO - useState with complex data structures

--- scrimbaReactPro/05-hooks/projecthooks2/src/App.js
+++ scrimbaReactPro/05-hooks/projecthooks2/src/App.js
@@ -4,24 +4,38 @@ function App() {
   const [inputData, setInputData] = useState({ firstName: '', lastName: '' });
   const [contactsData, setContactsData] = useState([]);
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setInputData((prevInputData) => ({ ...prevInputData, [name]: value }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  console.log(inputData);
+  console.log(contactsData);
+
   return (
     <div className='counter'>
-      <form onSubmit={}>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           placeholder='First Name:'
           name='firstName'
           value={inputData.firstName}
-          onChange={}
+          onChange={handleChange}
         />
         <input
           type='text'
           placeholder='Last Name:'
           name='lastName'
           value={inputData.lastName}
-          onChange={}
+          onChange={handleChange}
         />
+        <button type='submit'>Add contact</button>
       </form>
+      <h2>Contacts List</h2>
     </div>
   );
 }
