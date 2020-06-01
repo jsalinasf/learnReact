fe49317fbf50bffdde941235dab4b80b37f9a82f
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-05-31 23:04:15 -0500

    React PRO - useState with complex data structures

--- scrimbaReactPro/05-hooks/projecthooks2/src/App.js
+++ scrimbaReactPro/05-hooks/projecthooks2/src/App.js
@@ -1,47 +1,29 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function increment() {
-    setCount((prevCount) => prevCount + 1);
-  }
-
-  function decrement() {
-    setCount((prevCount) => prevCount - 1);
-  }
-
-  function reset() {
-    setCount(0);
-  }
+  const [inputData, setInputData] = useState({ firstName: '', lastName: '' });
+  const [contactsData, setContactsData] = useState([]);
 
   return (
     <div className='counter'>
-      <h1>{count}</h1>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={increment}>Increment</button>
+      <form onSubmit={}>
+        <input
+          type='text'
+          placeholder='First Name:'
+          name='firstName'
+          value={inputData.firstName}
+          onChange={}
+        />
+        <input
+          type='text'
+          placeholder='Last Name:'
+          name='lastName'
+          value={inputData.lastName}
+          onChange={}
+        />
+      </form>
     </div>
   );
 }
 
 export default App;
-
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       count: 0,
-//     };
-//   }
-//   render() {
-//     return (
-//       <div className='counter'>
-//         <h1>{this.state.count}</h1>
-//         <button>Change!</button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
