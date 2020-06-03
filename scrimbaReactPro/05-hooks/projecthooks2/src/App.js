358e24527289356cbe0db8f29a1521c92a7a2095
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-02 23:27:33 -0500

    Scrimba PRO - UseEffect 101

--- scrimbaReactPro/05-hooks/projecthooks2/src/App.js
+++ scrimbaReactPro/05-hooks/projecthooks2/src/App.js
@@ -1,49 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [inputData, setInputData] = useState({ firstName: '', lastName: '' });
-  const [contactsData, setContactsData] = useState([]);
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setInputData((prevInputData) => ({ ...prevInputData, [name]: value }));
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    setContactsData((prevContactsData) => [...prevContactsData, inputData]);
-    // Another way of doing it
-    // [...prevContactsData].concat(inputData)
-    setInputData({ firstName: '', lastName: '' });
-  }
-
-  const displayContacts = contactsData.map((contact) => (
-    <p key={contact.firstName + contact.lastName}>
-      {contact.firstName} {contact.lastName}
-    </p>
-  ));
-
   return (
-    <div className='counter'>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='First Name:'
-          name='firstName'
-          value={inputData.firstName}
-          onChange={handleChange}
-        />
-        <input
-          type='text'
-          placeholder='Last Name:'
-          name='lastName'
-          value={inputData.lastName}
-          onChange={handleChange}
-        />
-        <button type='submit'>Add contact</button>
-      </form>
-      <h2>Contacts List</h2>
-      {displayContacts}
+    <div>
+      <h1>Test</h1>
     </div>
   );
 }
