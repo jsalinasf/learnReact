84bc66a13ee5eb7f25c833e71fef27e00606175f
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-03 20:45:49 -0500

    ScrimbaPRO - Typing Game GUI compoleted

---
+++ scrimbaReactPro/05-hooks/projecthooks3/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>How fast can you type?</h1>
+      <textarea />
+      <h4>Time remaining:</h4>
+      <button>Start</button>
+      <h1>Word count is:</h1>
+    </div>
+  );
+}
+
+export default App;
