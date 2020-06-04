7189091f99d73bc7206512e59d1e261190c1c949
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-03 23:47:12 -0500

    ScrimbaPRO - Word count completed and re start game completed

--- scrimbaReactPro/05-hooks/projecthooks3/src/App.js
+++ scrimbaReactPro/05-hooks/projecthooks3/src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
+  const START_GAME = 2;
+
   const [text, setText] = useState('');
-  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [timeRemaining, setTimeRemaining] = useState(START_GAME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
   function handleChange(event) {
     const { value } = event.target;
@@ -17,19 +21,37 @@ function App() {
     return wordArr.length;
   }
 
+  function startGame() {
+    setTimeRemaining(START_GAME);
+    setText('');
+    setIsTimeRunning(true);
+  }
+
+  function stopGame() {
+    setWordCount(calculateWordCount(text));
+    setIsTimeRunning(false);
+  }
+
+  function handleClick() {
+    startGame();
+  }
+
   useEffect(() => {
-    if (timeRemaining > 0) {
+    if (timeRemaining > 0 && isTimeRunning) {
       setTimeout(() => setTimeRemaining((prevTime) => prevTime - 1), 1000);
+      console.log(isTimeRunning);
+    } else if (timeRemaining === 0) {
+      stopGame();
     }
-  }, [timeRemaining]);
+  }, [timeRemaining, isTimeRunning]);
 
   return (
     <div>
       <h1>How fast can you type?</h1>
       <textarea value={text} onChange={handleChange} />
       <h4>Time remaining: {timeRemaining}</h4>
-      <button>Start</button>
-      <h1>Word count is:</h1>
+      <button onClick={handleClick}>Start</button>
+      <h1>Word count: {wordCount}</h1>
     </div>
   );
 }
