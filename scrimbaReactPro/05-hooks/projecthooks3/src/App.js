9a8542b7785777e7bc60b518e34ab5eadec20456
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-06-03 20:55:51 -0500

    ScrimbaPRO - useState for TextArea, completed

--- scrimbaReactPro/05-hooks/projecthooks3/src/App.js
+++ scrimbaReactPro/05-hooks/projecthooks3/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setText(value);
+  }
+
   return (
     <div>
       <h1>How fast can you type?</h1>
-      <textarea />
+      <textarea value={text} onChange={handleChange} />
       <h4>Time remaining:</h4>
       <button>Start</button>
       <h1>Word count is:</h1>
