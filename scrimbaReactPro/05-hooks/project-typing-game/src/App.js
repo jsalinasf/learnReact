166b2508f9a5c775c4d4d5f9105b82c80d70f47b
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-09-30 18:44:35 -0500

    React PRO - Typing game basic functionality 101

--- scrimbaReactPro/05-hooks/project-typing-game/src/App.js
+++ scrimbaReactPro/05-hooks/project-typing-game/src/App.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+// Set the minimum length for words to be counted
+const minWordLength = 2;
+// set Initial Timer in seconds for the game
+const initialTimer = 5;
 
 const App = () => {
-  return <div>Test</div>;
+  const [text, setText] = useState('');
+  const [count, setCount] = useState('0');
+  const [timeRemaining, setTimeRemaining] = useState(initialTimer);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
+  function countWords(receivedText) {
+    let filteredWords = receivedText.trim().split(' ');
+    return filteredWords.filter((word) => word.length >= minWordLength).length;
+  }
+
+  function handleClick() {
+    startGame();
+  }
+
+  function startGame() {
+    setIsTimeRunning(true);
+    setTimeRemaining(initialTimer);
+    setCount(0);
+    setText('');
+  }
+
+  function endGame() {
+    setCount(countWords(text));
+    setIsTimeRunning(false);
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (isTimeRunning && timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, isTimeRunning]);
+
+  return (
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea value={text} onChange={handleChange} />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={handleClick}>Start</button>
+      <h1>word count: {count}</h1>
+    </div>
+  );
 };
 
 export default App;
