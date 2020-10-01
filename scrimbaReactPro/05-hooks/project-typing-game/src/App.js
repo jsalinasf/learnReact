7c5da2dda003d0c8404717c79b7c83173b339969
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-09-30 19:25:55 -0500

    React PRO - Typing game MVP ready

--- scrimbaReactPro/05-hooks/project-typing-game/src/App.js
+++ scrimbaReactPro/05-hooks/project-typing-game/src/App.js
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
 // Set the minimum length for words to be counted
-const minWordLength = 2;
+const MIN_WORD_LENGTH = 2;
 // set Initial Timer in seconds for the game
-const initialTimer = 5;
+const STARTING_TIME = 5;
 
 const App = () => {
   const [text, setText] = useState('');
-  const [count, setCount] = useState('0');
-  const [timeRemaining, setTimeRemaining] = useState(initialTimer);
+  const [wordCount, setWordCount] = useState('0');
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
 
   function handleChange(event) {
     setText(event.target.value);
   }
 
-  function countWords(receivedText) {
+  function calculateWordCount(receivedText) {
     let filteredWords = receivedText.trim().split(' ');
-    return filteredWords.filter((word) => word.length >= minWordLength).length;
+    return filteredWords.filter((word) => word.length >= MIN_WORD_LENGTH)
+      .length;
   }
 
   function handleClick() {
@@ -26,13 +27,13 @@ const App = () => {
 
   function startGame() {
     setIsTimeRunning(true);
-    setTimeRemaining(initialTimer);
-    setCount(0);
+    setTimeRemaining(STARTING_TIME);
+    setWordCount(0);
     setText('');
   }
 
   function endGame() {
-    setCount(countWords(text));
+    setWordCount(calculateWordCount(text));
     setIsTimeRunning(false);
   }
 
@@ -41,7 +42,7 @@ const App = () => {
       setTimeout(() => {
         setTimeRemaining((prevTime) => prevTime - 1);
       }, 1000);
-    } else if (isTimeRunning && timeRemaining === 0) {
+    } else if (timeRemaining === 0) {
       endGame();
     }
   }, [timeRemaining, isTimeRunning]);
@@ -49,10 +50,16 @@ const App = () => {
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea value={text} onChange={handleChange} />
+      <textarea
+        value={text}
+        onChange={handleChange}
+        disabled={!isTimeRunning}
+      />
       <h4>Time remaining: {timeRemaining}</h4>
-      <button onClick={handleClick}>Start</button>
-      <h1>word count: {count}</h1>
+      <button onClick={handleClick} disabled={isTimeRunning}>
+        Start
+      </button>
+      <h1>word count: {wordCount}</h1>
     </div>
   );
 };
