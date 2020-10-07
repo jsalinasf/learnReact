d0ff79eabf0bc042c589e38756274ec8acfbc7ba
jsalinasf <jorge.salinas.jsf@outlook.com>
2020-10-06 19:11:29 -0500

    ReactPRO - Hooks - useRef 102 project typing game

--- scrimbaReactPro/05-hooks/project-typing-game/src/App.js
+++ scrimbaReactPro/05-hooks/project-typing-game/src/App.js
@@ -1,24 +1,16 @@
-import React, { useState, useEffect } from 'react';
-
-// Set the minimum length for words to be counted
-const MIN_WORD_LENGTH = 2;
-// set Initial Timer in seconds for the game
-const STARTING_TIME = 5;
+import React, { useState, useEffect, useRef } from 'react';
 
 const App = () => {
+  const STARTING_TIME = 5;
+  const MIN_WORD_LENGTH = 2;
   const [text, setText] = useState('');
-  const [wordCount, setWordCount] = useState('0');
+  const [wordCount, setWordCount] = useState(0);
   const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const textAreaRef = useRef(null);
 
   function handleChange(event) {
     setText(event.target.value);
-  }
-
-  function calculateWordCount(receivedText) {
-    let filteredWords = receivedText.trim().split(' ');
-    return filteredWords.filter((word) => word.length >= MIN_WORD_LENGTH)
-      .length;
   }
 
   function handleClick() {
@@ -28,13 +20,21 @@ const App = () => {
   function startGame() {
     setIsTimeRunning(true);
     setTimeRemaining(STARTING_TIME);
-    setWordCount(0);
     setText('');
+    setWordCount(0);
+    textAreaRef.current.disabled = false;
+    textAreaRef.current.focus();
   }
 
-  function endGame() {
-    setWordCount(calculateWordCount(text));
+  function stopGame() {
+    setWordCount(countWords(text));
     setIsTimeRunning(false);
+  }
+
+  function countWords(receivedText) {
+    let filteredWords = receivedText.trim().split(' ');
+    return filteredWords.filter((word) => word.length >= MIN_WORD_LENGTH)
+      .length;
   }
 
   useEffect(() => {
@@ -43,7 +43,7 @@ const App = () => {
         setTimeRemaining((prevTime) => prevTime - 1);
       }, 1000);
     } else if (timeRemaining === 0) {
-      endGame();
+      stopGame();
     }
   }, [timeRemaining, isTimeRunning]);
 
@@ -51,17 +51,90 @@ const App = () => {
     <div>
       <h1>How fast do you type?</h1>
       <textarea
+        ref={textAreaRef}
         value={text}
         onChange={handleChange}
         disabled={!isTimeRunning}
-      />
+      ></textarea>
       <h4>Time remaining: {timeRemaining}</h4>
       <button onClick={handleClick} disabled={isTimeRunning}>
-        Start
+        START
       </button>
-      <h1>word count: {wordCount}</h1>
+      <h1>Word count: {wordCount}</h1>
     </div>
   );
 };
 
 export default App;
+
+// import React, { useState, useEffect, useRef } from 'react';
+
+// // Set the minimum length for words to be counted
+// const MIN_WORD_LENGTH = 2;
+// // set Initial Timer in seconds for the game
+// const STARTING_TIME = 5;
+
+// const App = () => {
+//   const [text, setText] = useState('');
+//   const [wordCount, setWordCount] = useState('0');
+//   const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+//   const [isTimeRunning, setIsTimeRunning] = useState(false);
+//   const textAreaRef = useRef(null);
+
+//   function handleChange(event) {
+//     setText(event.target.value);
+//   }
+
+//   function calculateWordCount(receivedText) {
+//     let filteredWords = receivedText.trim().split(' ');
+//     return filteredWords.filter((word) => word.length >= MIN_WORD_LENGTH)
+//       .length;
+//   }
+
+//   function handleClick() {
+//     startGame();
+//   }
+
+//   function startGame() {
+//     setIsTimeRunning(true);
+//     setTimeRemaining(STARTING_TIME);
+//     setWordCount(0);
+//     setText('');
+//     textAreaRef.current.disabled = false;
+//     textAreaRef.current.focus();
+//   }
+
+//   function endGame() {
+//     setWordCount(calculateWordCount(text));
+//     setIsTimeRunning(false);
+//   }
+
+//   useEffect(() => {
+//     if (isTimeRunning && timeRemaining > 0) {
+//       setTimeout(() => {
+//         setTimeRemaining((prevTime) => prevTime - 1);
+//       }, 1000);
+//     } else if (timeRemaining === 0) {
+//       endGame();
+//     }
+//   }, [timeRemaining, isTimeRunning]);
+
+//   return (
+//     <div>
+//       <h1>How fast do you type?</h1>
+//       <textarea
+//         ref={textAreaRef}
+//         value={text}
+//         onChange={handleChange}
+//         disabled={!isTimeRunning}
+//       />
+//       <h4>Time remaining: {timeRemaining}</h4>
+//       <button onClick={handleClick} disabled={isTimeRunning}>
+//         Start
+//       </button>
+//       <h1>word count: {wordCount}</h1>
+//     </div>
+//   );
+// };
+
+// export default App;
